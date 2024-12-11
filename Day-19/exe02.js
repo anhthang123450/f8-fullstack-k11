7f0c1adf8fcb298278fcc98aba320501738c5aa7
anhthang123450 <thangnd9803@gmail.com>
2024-12-11 17:25:48 +0700

    Nop bai tap ve nha

--- Day-19/exe02.js
+++ Day-19/exe02.js
@@ -1,9 +1,9 @@
-function printMultiplicationTable() {
-    for (let i = 2; i <= 9; i++) {
-        console.log(`Bảng cửu chương ${i}:`);
-        for (let j = 1; j <= 10; j++) {
-            console.log(`${i} x ${j} = ${i * j}`);
+function printMultiplicationTable(num) {
+    for (i = 1; i <= 9; i++) {
+        console.log(`Bảng cửu chương ${i} `);
+        for (j = 1; j <= 10; j++) {
+            console.log(`${i} * ${j} = ${j * i}`);
         }
     }
 }
-printMultiplicationTable();
+console.log(printMultiplicationTable());
