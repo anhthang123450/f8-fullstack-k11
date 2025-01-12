6f0bda4827202782b86fa3426cbe6bfe2afcd61d
anhthang123450 <thangnd9803@gmail.com>
2025-01-12 23:26:46 +0700

    Nop bai tap ve nha Day28

---
+++ Day-28/ex01.js
@@ -0,0 +1,14 @@
+function hourClock() {
+    const newYear = new Date("2026-01-01T00:00:00");
+    const now = new Date();
+    const totalSeconds = Math.floor((newYear - now) / 1000);
+    const days = Math.floor(totalSeconds / 86400);
+    const hours = Math.floor((totalSeconds % 86400) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    document.body.innerHTML = "";
+    document.write(
+        `Còn ${days} ngày ${hours} giờ ${minutes} phút ${seconds} là đến tết 2026 `
+    );
+}
+setInterval(hourClock, 1000);
